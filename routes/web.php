5586ac2368dbe68bfc63b6f9d99ef5d1e69f08c6
andrea-prina <and.prina@gmail.com>
2022-09-13 16:09:23 +0200

    Create Home and Comics controllers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('admin.home');
+Route::get('/comics', 'ComicController@index')->name('comics.index');
