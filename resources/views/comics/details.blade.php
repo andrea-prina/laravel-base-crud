d0508765f92dab719b74726506d7df665c6d2051
andrea-prina <and.prina@gmail.com>
2022-09-13 17:11:42 +0200

    Style comics details show view

--- resources/views/comics/details.blade.php
+++ resources/views/comics/details.blade.php
@@ -4,6 +4,14 @@
 
 @section('main-content')
 
-    <p>{{ $comic->title }}</p>
+    <div class="d-flex w-75">
+        <img src="{{ $comic->thumb }}" alt="">
+        <div class="ms-3">
+            <h3 class="fw-bolder">{{ $comic->title }}</h3>
+            <h4>{{ $comic->series }}</h4>
+            <p>{{ $comic->description }}</p>
+            <p class="fw-bold">{{ $comic->price . ' €'}}</p>
+        </div>
+    </div>
 
 @endsection
