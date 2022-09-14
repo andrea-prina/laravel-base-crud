a0d70253d2e434245e13bdd84f5cab36e385ef4a
andrea-prina <and.prina@gmail.com>
2022-09-14 16:51:36 +0200

    Add icon to header and link to home

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bolder" href="#">LARAVEL COMICS</a>
+        <a class="navbar-brand fw-bolder" href="{{ route('admin.home') }}"><i class="fa-solid fa-mask"></i> COMICS</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
